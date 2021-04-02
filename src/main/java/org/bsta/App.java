df37493f4b3370348bd17eabda9baedc06827bcf
Khalin <mpzx1102@gmail.com>
2021-04-02 16:12:55 +0900

    method test finished

--- src/main/java/org/bsta/App.java
+++ src/main/java/org/bsta/App.java
@@ -20,6 +20,7 @@ public class App
     static int count = 0;
     static List<String> realFileList = new ArrayList<>();
     static List<String> pathInFileList = new ArrayList<>();
+    static String regExp = "[a-zA-Z0-9_]*\\.jsp";
     public static void main( String[] args ) {
         
         //showFilesInDir(File.separator+"workspace"+File.separator+"fileFinder");
@@ -46,40 +47,41 @@ public class App
         //         break;
         //     }
         // }
-        String regExp = "[a-zA-Z0-9_]*\\.jsp";
-        if(file.getName().endsWith(".jsp")){
-            BufferedReader inFile =null;
-            try{
-                inFile = new BufferedReader(new FileReader(file));
-                String sLine = null;
-                while((sLine = inFile.readLine()) != null){
-                    String jspFile = "";
-                    //System.out.println(sLine);
-                    Pattern pattern = Pattern.compile(regExp);
-                    Matcher matcher = pattern.matcher(sLine);
-                    // System.out.println("matcher.find()" + matcher.find());
-                    while(matcher.find() != false){
-                        // if(matcher.group(1) == null){
-                        //     break;
-                        // }
-                        System.out.println(matcher.group(0));
-                    }
+        
+        // if(file.getName().endsWith(".jsp")){
+        //     BufferedReader inFile =null;
+        //     try{
+        //         inFile = new BufferedReader(new FileReader(file));
+        //         String sLine = null;
+        //         while((sLine = inFile.readLine()) != null){
+        //             String jspFile = "";
+        //             //System.out.println(sLine);
+        //             Pattern pattern = Pattern.compile(regExp);
+        //             Matcher matcher = pattern.matcher(sLine);
+        //             // System.out.println("matcher.find()" + matcher.find());
+        //             while(matcher.find() != false){
+        //                 // if(matcher.group(1) == null){
+        //                 //     break;
+        //                 // }
+        //                 System.out.println(matcher.group(0));
+        //             }
                     
-                }
-            }catch(IOException e){
-                e.printStackTrace();
-            }
+        //         }
+        //     }catch(IOException e){
+        //         e.printStackTrace();
+        //     }
 
-            if(inFile != null){
+        //     if(inFile != null){
 
-                try {
-                    inFile.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+        //         try {
+        //             inFile.close();
+        //         } catch (IOException e) {
+        //             e.printStackTrace();
+        //         }
+        //     }
 
-        }
+        // }
+        showPathInFile("C:"+File.separator+"Users"+File.separator+"LG"+File.separator+"Documents"+File.separator+"workspace-spring-tool-suite-4-4.8.0.RELEASE"+File.separator+"DSDM-Web"+File.separator+"WebApp"+File.separator+"sitemap");
     }
 
     //실제 존재하는 파일의 이름 저장
@@ -119,8 +121,22 @@ public class App
                     try{
                         inFile = new BufferedReader(new FileReader(file));
                         String sLine = null;
+                        // while((sLine = inFile.readLine()) != null){
+                        //     System.out.println(sLine);
+                        // }
                         while((sLine = inFile.readLine()) != null){
-                            System.out.println(sLine);
+                            String jspFile = "";
+                            //System.out.println(sLine);
+                            Pattern pattern = Pattern.compile(regExp);
+                            Matcher matcher = pattern.matcher(sLine);
+                            // System.out.println("matcher.find()" + matcher.find());
+                            while(matcher.find() != false){
+                                // if(matcher.group(1) == null){
+                                //     break;
+                                // }
+                                System.out.println(matcher.group(0));
+                            }
+                            
                         }
                     }catch(IOException e){
                         e.printStackTrace();
